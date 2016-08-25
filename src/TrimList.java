d19ba524eb41d1dc287e42884962fad475dc07cb
Austin Hendrix <bigahen@hotmail.com>
2016-08-25 13:58:13 -0400

    Application to trim words

---
+++ src/TrimList.java
@@ -0,0 +1,57 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.xml.soap.Text;
+
+public class TrimList {
+	public static void main(String args[]){
+		String document = "List of Words";
+		java.io.InputStream in = Text.class.getResourceAsStream(document + ".txt");
+		//BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		ArrayList<String> names = new ArrayList<String>();		
+		
+		try {
+			
+			Scanner inScanner = new Scanner(new FileReader("C:\\Users\\bigah\\workspace\\Finding Intelligence from Writing\\src\\List of Words.txt"));
+			while(inScanner.hasNextLine()){
+				names.add(inScanner.nextLine());
+			}
+			
+		} catch (FileNotFoundException e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		BufferedWriter writer = null;
+        try {
+            //create a temporary file
+            String fileString = "C:\\Users\\bigah\\workspace\\Finding Intelligence from Writing\\src\\List of Words 2.txt"; 
+            File file = new File(fileString);
+
+            // This will output the full path where the file will be written to...
+            System.out.println(file.getCanonicalPath());
+
+            writer = new BufferedWriter(new FileWriter(file));
+            
+            for(int i = 0; i < names.size(); i++){
+            	int index = names.get(i).indexOf(' ');
+                writer.write(names.get(i).substring(0, index));
+                writer.newLine();
+            }
+          
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+	}
+}
